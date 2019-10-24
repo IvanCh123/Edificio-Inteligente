5e1b275f73232c34cb5c2d5d87bfe1a13eb1c3ca
Luis Carmona Castro <b51558@ecci.ucr.ac.cr@ECCI386422L0104.ecci.ucr.ac.cr>
2019-10-24 12:02:07 -0600

    Implementación de la clase Cámara

---
+++ Seguridad/src/Camara.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.lang.Math;
+
+public class Camara {
+	
+	final String DIRECCIONES[] = {"izquierda", "centro", "derecha"};
+	int posicion[]= new int[2];
+	ArrayList<Integer> ids = new ArrayList<Integer>();
+	int direccion;
+	
+	private String getEstado() {
+		return "Cámara en torre " + getTorre() + ", piso " + getPiso() + " apuntando hacia " + getDireccion();
+	}
+	
+	private int getTorre() {
+		return this.posicion[0];
+	}
+	
+	private int getPiso() {
+		return this.posicion[1];
+	}
+	
+	private String getDireccion() {
+		return this.DIRECCIONES[this.direccion];
+	}
+	
+	private void setTorre(int torre) {
+		this.posicion[0] = torre;
+	}
+	
+	private void setPiso(int piso) {
+		this.posicion[1] = piso;
+	}
+	
+	private void setDireccion(int direccion) {
+		this.direccion = direccion;
+	}
+	
+	private void rotarIzquierda() {
+		this.direccion = Math.abs(--this.direccion)%3;
+	}
+	
+	private void rotarDerecha() {
+		this.direccion = ++this.direccion%3;
+	}
+}
