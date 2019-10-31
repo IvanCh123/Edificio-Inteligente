d7b054315803b4d4e7b430bff8385fa4726b3967
Pablo Cesar Ruiz Vasquez <b56404@ecci.ucr.ac.cr@ECCI386413L0104.ecci.ucr.ac.cr>
2019-10-31 12:44:35 -0600

    Log de entradas iniciado

--- Seguridad/src/ActividadManager.java
+++ Seguridad/src/ActividadManager.java
@@ -1,5 +1,19 @@
+import java.util.HashMap;
+
 public class ActividadManager{
 	
+	private HashMap<String, ActividadListener> listeners = new HashMap<String, ActividadListener>();
 	
+	public void suscribir(String evento, ActividadListener listener) {
+		listeners.put(evento, listener);
+	}
+	
+	public void cancelarSuscripcion(String evento, ActividadListener listener) {
+		listeners.remove(evento, listener);
+	}
+	
+	public void notificar(String evento, int id) {
+		
+	}
 	
 }
