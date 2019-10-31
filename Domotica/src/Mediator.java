454942b45ff1372ca6961a3c6d010bf00398fecd
Lucia <lesan2807@gmail.com>
2019-10-31 08:53:48 -0600

    LinkedHashMap para behaviors

--- Domotica/src/Mediator.java
+++ Domotica/src/Mediator.java
@@ -1,9 +1,12 @@
 import java.util.List; 
+import java.util.LinkedHashMap;
 
 public class Mediator {
 	
 	private List<Sensor> sensores; 
 	private List<Actuador> actuadores; 
+	private LinkedHashMap<Sensor,Actuador> comportamientos;
+	
 	
 	public void setSensores(List<Sensor> sensores)
 	{
