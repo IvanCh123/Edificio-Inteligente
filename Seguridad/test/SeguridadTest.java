4bee006c617582085e5762dbefab13b7cf0fb5e4
Luis Carmona Castro <b51558@ecci.ucr.ac.cr@ECCI386422L0104.ecci.ucr.ac.cr>
2019-10-31 12:00:09 -0600

    Primer Test Seguridad

---
+++ Seguridad/test/SeguridadTest.java
@@ -0,0 +1,20 @@
+import static junit.Assert.assertEquals;
+import junit.Test
+
+public class SeguridadTest{
+	@Test
+	public void buscarInquilinoTest(){
+		inquilinoId = 1;
+		SistemaSeguridad centroMando = SistemaSeguridad.getInstancia();
+		Camara camara = new CamaraPasillos();
+		centroMando.agregarCamara(camara);
+		camara.setTorre(1);
+		camara.setPiso(3);
+		camara.setDireccion(1);
+		camara.agregarVisualizado(inquilinoId);
+		Camara camaraTest = centroMando.buscarInquilino(inquilinoId);
+		String resultado = camaraTest.getEstado();
+		String respuesta = camara.getEstado();
+		assertEquals(resultado, respuesta);
+	}
+}
