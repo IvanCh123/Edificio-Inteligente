245360ce62c49816c8e9456d6f75086a3554e93a
Lucia E <lesan2807@gmail.com>
2019-10-31 20:51:30 -0600

    add behavior

--- Domotica/src/Mediator.java
+++ Domotica/src/Mediator.java
@@ -1,14 +1,20 @@
 import java.util.List; 
+import java.util.ArrayList;
+
 import java.util.HashMap;
 
 public class Mediator {
 	
 	private List<Sensor> sensores; 
 	private List<Actuador> actuadores; 
+	
 	private HashMap<String, List<TramientoComportamiento>> comportamientos;
 	
-	private boolean senal; 
 	
+	public Mediator()
+	{
+		
+	}
 	
 	public void setSensores(List<Sensor> sensores)
 	{
@@ -22,28 +28,20 @@ public class Mediator {
 	
 	public void addComportamiento(String commandNombre, TramientoComportamiento command ){
 		
-		// crear lista y agregar el comportamiento a la lista para poder agregar al mapa
-		// comportamientos.add(commandNombre, command);		
+		if ( comportamientos.get(commandNombre) == null)
+		{
+			comportamientos.put(commandNombre, new ArrayList<TramientoComportamiento>()); 
+		}
+		
+		comportamientos.get(commandNombre).add(command); 
 	}
 	
-	public void setSenal(boolean senal)
-	{
-		this.senal = senal; 
-	}
-	
-	public boolean getSenal() {
-		return this.senal; 
-	}
 	
 	public void ejecutarComportamiento(String commandNombre){
 		
-		if( this.getSenal() == true)
-		{
-			for(int index = 0; index <comportamientos.get(commandNombre).size(); ++index){
-				// Recorrer y ejecutar
-			}
+		for(int index = 0; index < comportamientos.get(commandNombre).size(); ++index){
+			comportamientos.get(commandNombre).get(index).ejecutar();
 		}
-
 	}
 }
 
