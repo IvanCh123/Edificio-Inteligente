e16056d9a90a9e6ec82d1a1a3b16cba3427c6c2c
felipecarmonaucr <luis.carmonacastro@ucr.ac.cr>
2019-11-07 05:47:08 -0600

    Actualizacion Proxy

--- Seguridad/src/InternetProxy.java
+++ Seguridad/src/InternetProxy.java
@@ -1,10 +1,24 @@
+import java.util.ArrayList;
 
 public class InternetProxy implements InternetInterface{
 
+	int limite = 100;
+	ArrayList<String> paginasRestringidas;
 	@Override
 	public void acceder(int idRegistro, String pagina) {
-		// TODO Auto-generated method stub
-		
+		InternetManager servicio = new InternetManager();
+		if(servicio.registros.get(idRegistro).size()>=limite) {
+			if(!this.paginasRestringidas.contains(pagina)) {
+				servicio.acceder(idRegistro, pagina);
+			}else {
+				System.out.print("La página que está intentando acceder no está permitida");
+			}
+		}else {
+			System.out.print("Usted ha exedido el límite de páginas que puede acceder");
+		}
 	}
 	
+	public void agregarRestriccion(String restriccion) {
+		this.paginasRestringidas.add(restriccion);
+	}
 }
