e73abb794c9dbccff8ac269a583cf83d618e8d91
Lucia E <lesan2807@gmail.com>
2019-11-25 18:22:10 -0600

    Builder para el edificio

--- Dispatch/src/Edificio.java
+++ Dispatch/src/Edificio.java
@@ -1,4 +1,39 @@
+import java.util.List; 
+import java.util.ArrayList;
 
 public class Edificio {
 
+	private int numeroDePisos; 
+	private List<Elevador> elevadores; 
+	
+	private Edificio() {
+		elevadores = new ArrayList<Elevador>(); 
+	}
+	
+	public static class Builder
+	{
+		private Edificio edificio; 
+		
+		public Builder()
+		{
+			edificio = new Edificio(); 
+		}
+		
+		public Builder setNumeroDePisos(int numeroDePisos)
+		{
+			edificio.numeroDePisos = numeroDePisos; 
+			return this; 
+		}
+		
+		public Builder setElevadores(List<Elevador> elevadores)
+		{
+			edificio.elevadores.addAll(elevadores); 
+			return this; 
+		}
+		
+		public Edificio crea()
+		{
+			return edificio; 
+		}
+	}
 }
--- Dispatch/src/Elevador.java
+++ Dispatch/src/Elevador.java
@@ -1,4 +1,15 @@
 
 public class Elevador {
-
+	
+	public enum ElevatorDirection {
+		 ELEVATOR_UP,
+		 ELEVATOR_DOWN,
+		 ELEVATOR_HOLD
+	}
+	
+	public enum ElevatorStatus {
+		  ELEVATOR_OCCUPIED,
+		  ELEVATOR_EMPTY;
+	}
+	
 }
